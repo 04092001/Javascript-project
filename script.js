9bf2c37fd2c6e78d5acb3c0f15cc75553bc4f682
04092001 <fheath090401@gmail.com>
2023-03-08 16:01:55 +0000

    modified:   index.html 	modified:   script.js 	modified:   stylesheet.css

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-const todos = [];
-
-render();
-
-function addTodo() {
-  const textbox = document.getElementById("todo-title");
-  const title = textbox.value;
-
-  const datePicker = document.getElementById("date-picker");
-  const dueDate = datePicker.value;
-  todos.push(title);
-
-  render();
+function expand() {
+  document.getElementById("section").style.display = "block";
 }
 
-function render() {
-  // reset our list
-  document.getElementById("todo-list").innerHTML = "";
-
-  todos.forEach(function (todoTitle) {
-    const element = document.createElement("div");
-    element.innerText = todoTitle;
-    const todoList = document.getElementById("todo-list");
-    todoList.appendChild(element);
-  });
+function collapse() {
+  document.getElementById("section").style.display = "none";
 }
